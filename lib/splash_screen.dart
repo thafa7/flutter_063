42b983ba516c6eb4f880a3e329968b40f23a06b0
thafa7 <musthafaanggoro07@gmail.com>
2024-10-31 13:24:10 +0700

    hello

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_063/main.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScree extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
